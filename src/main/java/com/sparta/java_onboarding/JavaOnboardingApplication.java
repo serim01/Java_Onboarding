1af2d43f3aff3755b665937e95504d6dedc3c819
serim01 <kkhksl13@gmail.com>
2024-10-08 18:34:48 +0900

    git init

---
+++ src/main/java/com/sparta/java_onboarding/JavaOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.java_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/java_onboarding/JavaOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.java_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
