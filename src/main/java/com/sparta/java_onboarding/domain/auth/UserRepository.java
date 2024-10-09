133483c401f51547f1e4eb23fe5d6b187452fccd
serim01 <kkhksl13@gmail.com>
2024-10-10 02:24:57 +0900

    [#3]feat:sign api - Controller - Service - Repository

--- src/main/java/com/sparta/java_onboarding/domain/auth/UserRepository.java
+++ src/main/java/com/sparta/java_onboarding/domain/auth/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sparta.java_onboarding.domain.auth.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	User findUserByUsername(String username);
 }
