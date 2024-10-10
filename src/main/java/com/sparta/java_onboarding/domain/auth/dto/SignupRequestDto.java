8d754f4f3af03dc68decf22db7df1149bae76794
serim01 <kkhksl13@gmail.com>
2024-10-10 21:52:12 +0900

    [#5]refactor:test dto - test 위한 DTO 수정

--- src/main/java/com/sparta/java_onboarding/domain/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/java_onboarding/domain/auth/dto/SignupRequestDto.java
@@ -7,4 +7,10 @@ public class SignupRequestDto {
 	private String username;
 	private String nickname;
 	private String password;
+
+	public SignupRequestDto(String username, String nickname, String password) {
+		this.username = username;
+		this.nickname = nickname;
+		this.password = password;
+	}
 }
