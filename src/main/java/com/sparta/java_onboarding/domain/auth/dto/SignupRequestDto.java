3b12ef5c66e14078af02aa0680344fa1fdae776a
serim01 <kkhksl13@gmail.com>
2024-10-11 02:25:12 +0900

    [#9]fix:requestdto noargsConstructor 추가

--- src/main/java/com/sparta/java_onboarding/domain/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/java_onboarding/domain/auth/dto/SignupRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.java_onboarding.domain.auth.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SignupRequestDto {
 	private String username;
 	private String nickname;
